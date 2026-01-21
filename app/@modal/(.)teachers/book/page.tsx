b4e08a1e9bb9f22e4be0d9737e42cc840ae8ad2d
Sergey-8057 <erste_sergey@proton.me>
2026-01-21 23:52:01 +0200

    edit-3 Modal book

--- app/@modal/(.)teachers/book/page.tsx
+++ app/@modal/(.)teachers/book/page.tsx
@@ -1,6 +1,6 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import BookClient from './BookClient';
+const BookClient = dynamic(() => import('./BookClient'), { ssr: false });
 
 export default function BookModalPage() {
   return <BookClient />;
