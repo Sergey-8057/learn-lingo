c35d16c881e5b4071babb5da635a49075f31e393
Sergey-8057 <erste_sergey@proton.me>
2026-01-22 00:00:42 +0200

    edit-4 Modal book

--- app/@modal/(.)teachers/book/BookClient.tsx
+++ app/@modal/(.)teachers/book/BookClient.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import Modal from '@/components/Modal/Modal';
-import Book from '@/components/Book/Book';
+import dynamic from 'next/dynamic';
+
+const Book = dynamic(() => import('@/components/Book/Book'), { ssr: false });
 
 export default function BookClient() {
   return (
