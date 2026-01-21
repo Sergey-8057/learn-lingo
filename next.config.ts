bd8ceedd4ab5a7e21aeb88fb3471c02ba896b414
Sergey-8057 <erste_sergey@proton.me>
2026-01-21 23:17:49 +0200

    edit next.config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'ftp.goit.study' }],
   },
-  basePath: '/learn-lingo',
-  assetPrefix: '/learn-lingo/',
 };
 
 export default nextConfig;
