f223db40b5476b3b3c20e2fe8a5f12558aa53513
Sergey-8057 <erste_sergey@proton.me>
2026-01-21 23:08:18 +0200

    del loader

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'ftp.goit.study' }],
   },
+  basePath: '/learn-lingo',
+  assetPrefix: '/learn-lingo/',
 };
 
 export default nextConfig;
