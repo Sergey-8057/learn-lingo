e6c814c6fe81675a44bdded71528945220f0ff1d
Sergey-8057 <erste_sergey@proton.me>
2026-01-28 01:02:55 +0200

    edit header.css

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'ftp.goit.study' }],
-  },
-  experimental: {
-    optimizeCss: false,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
   },
 };
 
